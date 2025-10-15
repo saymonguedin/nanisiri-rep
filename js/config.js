95a0fab1040eabda6ed6846908fc1d9f62e3044f
saymonguedin <kankshithsrinu@gmail.com>
2025-10-15 13:24:56 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_8FPn0swyc', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '27kg5moior3vgp63622vhj7q1t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_pzlEuNrS5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7fe3pcmf9t206tr0ls3a2sd726', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
